caa39e42af342b0f0b67cb1eb46662fd194355f8
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-03-28 14:58:31 +0300

    Default Widget

--- SimpleWidget/SimpleWidget.swift
+++ SimpleWidget/SimpleWidget.swift
@@ -21,18 +21,16 @@ struct Provider: TimelineProvider {
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of seven entries a day apart, starting from the current day.
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for dayOffset in 0..<7{
-            let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
             let entry = SimpleEntry(date: entryDate)
-                    entries.append(entry)
-            
+            entries.append(entry)
         }
-        
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-           completion(timeline)
 
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
     }
 }
 
@@ -40,6 +38,13 @@ struct SimpleEntry: TimelineEntry {
     let date: Date
 }
 
+struct SimpleWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
 
 @main
 struct SimpleWidget: Widget {
@@ -54,50 +59,6 @@ struct SimpleWidget: Widget {
     }
 }
 
-struct SimpleWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        ZStack{
-            VStack(spacing:5){
-        Text(getMonth())
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(.horizontal,20)
-                    .background(Rectangle().fill(Color.red))
-                Text(getDay())
-                Text(getDate())
-                    .font(.largeTitle)
-
-            }
-            .background(Rectangle().fill(Color("WidgetBackground")))
-            .padding(20)
-            .clipShape(Circle())
-            .shadow(color: .gray, radius: 5, x: 5, y: 5)
-            
-        }.ignoresSafeArea()
-           
-    }
-    
-    private func getDay()->String{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "E"
-        return dateFormatter.string(from:entry.date)
-        
-    }
-    private func getDate()->String{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd"
-        return dateFormatter.string(from:entry.date)
-        
-    }
-    private func getMonth()->String{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM"
-        return dateFormatter.string(from:entry.date)
-        
-    }
-}
 struct SimpleWidget_Previews: PreviewProvider {
     static var previews: some View {
         SimpleWidgetEntryView(entry: SimpleEntry(date: Date()))
