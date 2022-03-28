389b33fd8dd54ce1d90e96546ff26ce244104d48
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-03-28 13:58:16 +0300

    Initial Commit

---
+++ WidgetKitExploredApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetKitExploredApp
+//
+//  Created by khawlah khalid on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetKitExploredApp/WidgetKitExploredAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitExploredAppApp.swift
+//  WidgetKitExploredApp
+//
+//  Created by khawlah khalid on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitExploredAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
